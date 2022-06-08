9757237548254709a12aa32ceaabd4006d390649
zach2039 <sumyunguy2007@gmail.com>
2022-06-07 19:28:20 -0500

    Remove monomod patch attempt and move to Harmony

--- Assets/Scripts/Atmospherics/Atmosphere_Patch.cs
+++ Assets/Scripts/Atmospherics/Atmosphere_Patch.cs
@@ -187,6 +187,19 @@ namespace Assets.Scripts.Atmospherics
 			gasMixture.UpdateCache();
 			return false; // skip original method
 		}
+
+		[HarmonyPatch("ParticalPressureO2", MethodType.Setter)]
+		public float ParticalPressureO2
+		{
+			get
+			{
+				if (this.IsCachable && !Atmosphere.CanWriteAccess)
+				{
+					return this._particalPressureO2Cached;
+				}
+				return this.GetAdditionalData().ModGasMixture.GetGasByName("oxygen").Quantity * 8.3144f * this.GetAdditionalData().ModGasMixture.Temperature / this.Volume;
+			}
+		}
 	}
 }
 
